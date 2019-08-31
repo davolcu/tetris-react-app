a508f82af5b905b76fac81afc788de2c9260f686
Dav <dav@MacBook-Air-de-Dav.local>
2019-08-31 07:47:36 +0200

    Rotate pieces and clear rows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -12,7 +12,7 @@ export const useStage = (tetromino, resetTetromino) => {
 
         const sweepRows = newStage =>
             newStage.reduce((ack, row) => {
-                if (row.findIndex(cell => cell[0] === 0) !== -1) {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
                     setRowsCleared(prev => prev + 1);
                     ack.unshift(new Array(newStage[0].length).fill([0, "clear"]));
                     return ack;
